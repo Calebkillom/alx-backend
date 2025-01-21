2abe56c0b492080263865a92619490285f979b24
Calebkillom <Calebkillom@gmail.com>
2025-01-22 02:36:08 +0300

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- Coding: utf-8 -*-
 """
-Create a class BasicCache that inherits from BaseCaching and is a caching system:
+Create a class BasicCache that inherits from BaseCaching
+and is a caching system:
 """
 from base_caching import BaseCaching
 
